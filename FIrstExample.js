84641a6c20fe603a11997becf926e323cbb3c950
blackbryant <e49134030e@gmail.com>
2024-11-30 09:24:08 +0800

    跑壓力測試看看

--- FIrstExample.js
+++ FIrstExample.js
@@ -2,12 +2,14 @@
     主題: 簡單測試範例
     內容: Get一個網址內容
     安裝教學: https://blog.miniasp.com/post/2023/08/01/Getting-Started-with-Grafana-k6-Load-testing-tool (保哥)
+    執行1: k6 run FIrstExample.js
+    執行2(利用 10 VUs 進行為期 30 秒的壓力測試): k6 run --vus 10 --duration 30s test.js
 */
 
 import http from "k6/http";
 import { sleep } from "k6";
 
 export default function () {
-  http.get("https://test.k6.io");
+  http.get("http://localhost:9999");
   sleep(1);
 }
