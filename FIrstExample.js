d788ee320f080707d82bcf219cbbb1f990bed187
blackbryant <e49134030e@gmail.com>
2024-11-30 10:08:31 +0800

    新增使用狀態碼來記錄錯誤的原因

--- FIrstExample.js
+++ FIrstExample.js
@@ -2,8 +2,9 @@
     主題: 簡單測試範例
     內容: Get一個網址內容
     安裝教學: https://blog.miniasp.com/post/2023/08/01/Getting-Started-with-Grafana-k6-Load-testing-tool (保哥)
-    執行1: k6 run FIrstExample.js
-    執行2(利用 10 VUs 進行為期 30 秒的壓力測試): k6 run --vus 10 --duration 30s test.js
+    執行1: k6 run FirstExample.js
+    執行2(利用 10 VUs 進行為期 30 秒的壓力測試): k6 run --vus 10 --duration 30s FirstExample.js
+    執行3(利用 10 VUs 進行總共 100 次 iterations 的壓力測試):k6 run --vus=10 --iterations=100 FirstExample.js
 */
 
 import http from "k6/http";
---
+++ FirstAdvncedExample.js
@@ -0,0 +1,18 @@
+/*
+    主題: 簡單測試範例
+    內容: Get一個網址內容，在腳本中記錄每種 HTTP 狀態碼的分佈（如 429, 200, 等），以更清楚地了解失敗原因。
+         當超過負載會回傳503狀態碼，我們會在訊息中顯示http_503_responses
+    執行1(利用 10 VUs 進行為期 30 秒的壓力測試): k6 run --vus 10 --duration 30s FirstExample.js
+*/
+
+import http from "k6/http";
+import { Counter } from "k6/metrics";
+
+let status503 = new Counter("http_503_responses");
+
+export default function () {
+  let res = http.get("http://localhost:9999");
+  if (res.status === 503) {
+    status503.add(1);
+  }
+}
